f74cdb5ed2698868778f1ff31772da3a15c23937
Ali-Shariyat <a.shariyat.78.99.20@gmail.com>
2020-01-19 22:00:03 +0330

    create address purchase page.

--- assets/js/main.js
+++ assets/js/main.js
@@ -493,3 +493,34 @@ $(document).on("click",".va-nav",function (e) {
 //         })
 //     })
 // })
+$(".edit-form").click(function (e) {
+    $(this).parents("form").find("[data-switch-disabled]").each(function () {
+        if ($(this).hasAttr("disabled")) {
+            $(this).removeAttr("disabled");
+        }else{
+            $(this).attr("disabled","disabled");
+        }
+    });
+    $(this).addClass("disabled");
+    e.stopPropagation();
+    e.preventDefault();
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+    window.addEventListener('load', function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+            form.addEventListener('submit', function (event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
